94d0ee4547aa996348047056148aa35d4d4dfc64
colin <colin.williams.dev@gmail.com>
2023-10-30 10:43:50 -0500

    feat: add DateTime timestamp to Blog model

--- Models/Model.cs
+++ Models/Model.cs
@@ -6,6 +6,7 @@ public class Blog
 {
     public int BlogId { get; set; }
     public string Url { get; set; }
+    public DateTime CreatedTimestamp { get; set; }
 
     public List<Post> Posts { get; } = new();
 }
