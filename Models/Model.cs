6cfab86f6bee9a0987038051e2e6799761b87abb
colin <colin.williams.dev@gmail.com>
2023-10-30 10:51:31 -0500

    chore: add docs link migrations EF

--- Models/Model.cs
+++ Models/Model.cs
@@ -44,3 +44,5 @@ public class Post
  * apply migration:
  * `dotnet ef database update`
  */
+
+/* See [EF Migration Docs](https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli) */
